c14e2655bf26bef891eafa0db37ee22de04197bb
Alekiel42 <alexandra.bercoff@gmail.com>
2021-06-03 15:12:30 +0200

    handle click on square

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -2,18 +2,21 @@ import React, { useState } from "react";
 import Grid from "./Grid";
 
 const Matrix = () => {
-  // tableau avec ordre des valeurs ? 
   const matrixSymbols = ['', 'O', 'V', 'X', 'H'];
 
   const [symbolOnSquare, setSymbolOnSquare] = useState(Array(9).fill(matrixSymbols[0]));
 
   const handleClick = (index) => {
-    // index du tableau symbolOnSquare
-    // recup valeur du tableau
-    // inbdex of par rapport a matrixSympa
-    // si indexOf > 4 alors 0 sinon: 
-    // +1
-    // update juste element de cet index avec set
+    const copySymbolOnSquare = [...symbolOnSquare];
+
+    const indexMatrixSymbol = matrixSymbols.indexOf(copySymbolOnSquare[index]);
+    if(indexMatrixSymbol === matrixSymbols.length -1 ) {
+      copySymbolOnSquare[index] = matrixSymbols[0]; 
+      setSymbolOnSquare(copySymbolOnSquare); 
+    } else {
+      copySymbolOnSquare[index] = matrixSymbols[indexMatrixSymbol + 1]; 
+      setSymbolOnSquare(copySymbolOnSquare); 
+    }
   };
 
   return (
