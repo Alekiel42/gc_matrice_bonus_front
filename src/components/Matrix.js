3fddc2d6e4c6ded0422aa4293cfaeba5ef0a5eac
Alekiel42 <alexandra.bercoff@gmail.com>
2021-06-03 19:12:39 +0200

    display / not display grid and button

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -5,68 +5,84 @@ import Info from "./Info";
 
 const Matrix = () => {
   const matrixSymbols = ['', 'O', 'V', 'X', 'H'];
+  let gameDisplayed = null;
   const [symbolOnSquare, setSymbolOnSquare] = useState(Array(9).fill(matrixSymbols[0]));
   const [info, setInfo] = useState('');
+  const [isDisplayed, setIsDisplayed] = useState(true);
 
-  const handleClick = (index) => {
-    const copySymbolOnSquare = [...symbolOnSquare];
-    const indexMatrixSymbol = matrixSymbols.indexOf(copySymbolOnSquare[index]);
+    const handleClick = (index) => {
+      const copySymbolOnSquare = [...symbolOnSquare];
+      const indexMatrixSymbol = matrixSymbols.indexOf(copySymbolOnSquare[index]);
 
-    if(indexMatrixSymbol === matrixSymbols.length -1 ) {
-      copySymbolOnSquare[index] = matrixSymbols[0]; 
-      setSymbolOnSquare(copySymbolOnSquare); 
-    } else {
-      copySymbolOnSquare[index] = matrixSymbols[indexMatrixSymbol + 1]; 
-      setSymbolOnSquare(copySymbolOnSquare); 
+      if (indexMatrixSymbol === matrixSymbols.length - 1) {
+        copySymbolOnSquare[index] = matrixSymbols[0];
+        setSymbolOnSquare(copySymbolOnSquare);
+      } else {
+        copySymbolOnSquare[index] = matrixSymbols[indexMatrixSymbol + 1];
+        setSymbolOnSquare(copySymbolOnSquare);
+      }
+    };
+
+    const handleButton = async () => {
+      try {
+        let resultToSend = '';
+        const copySymbolOnSquare = [...symbolOnSquare];
+
+        for (const symbol of copySymbolOnSquare) {
+          const indexMatrixSymbol = matrixSymbols.indexOf(symbol);
+          resultToSend += indexMatrixSymbol;
+        }
+
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ matrice: resultToSend }),
+        };
+
+        const response = await fetch("http://localhost:4000/answer", requestOptions);
+
+        if (response.status === 201) {
+          const result = await response.json();
+          setInfo(`La rapport semble juste Agent. Rendez-vous aux coordonnées ${result.gps} pour confirmer votre position.`);
+          setIsDisplayed(false);
+        } else if (response.status === 202) {
+          const result = await response.json();
+          setInfo(result.message);
+        } else {
+          throw await response.json();
+        }
+
+      } catch (error) {
+        console.error(error);
+      }
+
     }
+
+    if (isDisplayed) {
+      gameDisplayed = (
+        <div>
+          <Grid symbols={symbolOnSquare} onClick={handleClick} />
+          <Button onClick={handleButton} />
+        </div>
+      );
+    }
+
+    return (
+      <main>
+        <h1>Matrix - GC</h1>
+        <div>
+          <p>
+            Cette interface est liée à la cache Mystery : XXXXXX.
+        </p>
+          <p>
+            Pour envoyer votre rapport via cette interface il vous faut avoir trouver les failles de la matrice : <a href="https://www.geocaching.com/geocache/GC9BNJM_matrice-the-other-side" target="_blank" rel="noreferrer">GC9BNJM</a>, <a href="https://www.geocaching.com/geocache/GC9BNMJ_matrice-which-side" target="_blank" rel="noreferrer">GC9BNMJ</a> et <a href="https://www.geocaching.com/geocache/GC9BNM2_matrice-inside" target="_blank" rel="noreferrer">GC9BNM2</a>.
+        </p>
+
+        </div>
+        {gameDisplayed}
+        <Info info={info} />
+      </main>
+    );
   };
 
-  const handleButton = async () => {
-    try{
-          let resultToSend = '';
-          const copySymbolOnSquare = [...symbolOnSquare];
-      
-          for(const symbol of copySymbolOnSquare) {
-            const indexMatrixSymbol = matrixSymbols.indexOf(symbol);
-            resultToSend += indexMatrixSymbol;
-          }
-          
-          const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ matrice : resultToSend }),
-          };
-      
-          const response =  await fetch("http://localhost:4000/answer",  requestOptions);
-      
-          if (response.status === 201) {
-            const result = await response.json();
-            setInfo(result.gps);
-            console.log('GPS juste', result.gps);
-          } else if (response.status === 202) {
-            const result = await response.json();
-            // afficher component
-            setInfo(result.message);
-            console.log('faux', result.message);
-          } else {
-            throw await response.json();
-          }
-          
-    } catch(error){
-      console.error(error);
-    }
-
-
-  }
-
-  return (
-    <div>
-      <h1>Matrix - GC</h1>
-      <Grid symbols={symbolOnSquare} onClick={handleClick} />
-      <Button onClick={handleButton} />
-      <Info info={info} />
-    </div>
-  );
-};
-
-export default Matrix;
+  export default Matrix;
